09efee9262822715f618b676fb596ce2b49f49d1
ximenwuque <ximenwuque@126.com>
2023-02-01 16:29:20 +0800

    导出产品 添加字段

--- zkzl-module-pro/zkzl-module-pro-biz/src/main/java/com/zkzl/module/pro/controller/admin/product/vo/ProductExcelVO2.java
+++ zkzl-module-pro/zkzl-module-pro-biz/src/main/java/com/zkzl/module/pro/controller/admin/product/vo/ProductExcelVO2.java
@@ -19,18 +19,71 @@ import java.util.Date;
 @Data
 public class ProductExcelVO2 {
 
+    @ExcelProperty("产品名称")
+    @ContentLoopMerge(columnExtend = 2)
+    @ColumnWidth(value = 18)
+    private String productNameCn;
+
+    @ExcelProperty("产品名称")
+    @ColumnWidth(value = 18)
+    private String productNameEn;
+
     @ExcelProperty("包装方式")
     @ContentLoopMerge(columnExtend = 2)
     @ColumnWidth(value = 18)
     private String packagingMethodCn;
 
+    @ExcelProperty("包装方式")
+    @ColumnWidth(value = 18)
+    private String packagingMethodEn;
+
     @ExcelProperty("图片")
     @ColumnWidth(value = 38)
     private File pic;
 
+    @ExcelProperty("产品型号")
+    @ColumnWidth(value = 18)
+    private String productModel;
+
+    @ExcelProperty("颜色")
+    @ColumnWidth(value = 18)
+    @ContentLoopMerge(columnExtend = 2)
+    private String colourCn;
+
+    @ExcelProperty("颜色")
+    @ColumnWidth(value = 18)
+    private String colourEn;
+
+    @ExcelProperty("用途分类")
+    @ColumnWidth(value = 18)
+    @ContentLoopMerge(columnExtend = 2)
+    private String useClassificationCn;
+
+    @ExcelProperty("用途分类")
+    @ColumnWidth(value = 18)
+    private String useClassificationEn;
+
+    @ExcelProperty("交货方式")
+    @ColumnWidth(value = 18)
+    @ContentLoopMerge(columnExtend = 2)
+    private String deliveryMethodCn;
+
+    @ExcelProperty("交货方式")
+    @ColumnWidth(value = 18)
+    private String deliveryMethodEn;
+
+    @ExcelProperty("HS编码")
+    @ColumnWidth(value = 18)
+    private String hsNo;
+
+
     @ExcelProperty("克重")
     @ColumnWidth(value = 18)
     private String gramWeight;
+
+    @ExcelProperty("尺寸")
+    @ColumnWidth(value = 18)
+    private String size;
 
     @ExcelProperty("箱规")
     @ColumnWidth(value = 18)
